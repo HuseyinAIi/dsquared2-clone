95a4e86fa082b60f2e56b7738b985db69c14f560
HuseyinAIi <huseyinaliyigitt@gmail.com>
2025-08-21 14:54:38 +0300

    Add client/src/components/Footer.js 1755777278535

---
+++ client/src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="footer-content">
+          <p>&copy; 2024 DSQUARED2. All rights reserved.</p>
+          <p>Luxury fashion for the modern individual</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
